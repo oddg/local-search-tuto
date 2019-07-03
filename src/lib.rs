b7b730fff64f8665bad6753e043a7cd40cae1d08
oddg <odegevigney@gmail.com>
2019-07-03 15:04:37 -0700

    Draft algo and viewer

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,69 @@
+use lazy_static::lazy_static;
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+use std::sync::Mutex;
 use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
 
-pub struct Model {}
+mod data;
+
+const MAX_VALUE: u32 = 10000;
+
+fn color(v: u32) -> u32 {
+    (((MAX_VALUE - v) as f32 / MAX_VALUE as f32) * 240.0) as u32
+}
+
+lazy_static! {
+    static ref RNG: Mutex<SmallRng> = Mutex::new(SmallRng::seed_from_u64(777));
+}
+
+struct Problem {
+    size: usize,
+    values: Vec<Vec<u32>>,
+}
+
+struct Grid {
+    selected: Option<Position>,
+}
+
+type Position = (usize, usize);
+struct Algo {
+    current_position: Position,
+    neighboors: Vec<Position>,
+}
+
+impl Algo {
+    fn new(p: Position) -> Self {
+        Algo {
+            current_position: p,
+            neighboors: vec![],
+        }
+    }
+
+    fn next(&mut self, problem: &Problem) -> AlgoEvents {
+        let mut rng = RNG.lock().unwrap();
+        AlgoEvents::SelectedSolution((
+            rng.gen_range(0, problem.size),
+            rng.gen_range(0, problem.size),
+        ))
+    }
+}
+
+enum AlgoEvents {
+    // The position is the current best known solution
+    SelectedSolution(Position),
+    // The algo will explore the neighborhood
+    SelectedNeighborhood(Vec<Position>),
+    // The algo evaluated the position
+    Evaluated(Position),
+}
+
+pub struct Model {
+    problem: Problem,
+    algo: Algo,
+    grid: Grid,
+}
 
 pub enum Msg {
-    Click,
+    Next,
 }
 
 impl Component for Model {
@@ -11,12 +71,31 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Model {}
+        let size = 70;
+        let problem = Problem {
+            size,
+            values: data::VALUES[0..size]
+                .iter()
+                .map(|v| v[0..size].to_vec())
+                .collect(),
+        };
+        let grid = Grid { selected: None };
+        let algo = Algo::new((0, 0));
+        Model {
+            problem,
+            grid,
+            algo,
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::Click => {}
+            Msg::Next => match self.algo.next(&self.problem) {
+                AlgoEvents::SelectedSolution(p) => {
+                    self.grid.selected = Some(p);
+                }
+                _ => unreachable!(),
+            },
         }
         true
     }
@@ -26,8 +105,31 @@ impl Renderable<Model> for Model {
     fn view(&self) -> Html<Self> {
         html! {
             <div>
-                <button onclick=|_| Msg::Click,>{ "Click" }</button>
+                <div id="control",>
+                  <button onclick=|_| Msg::Next,>{ "Next" }</button>
+                </div>
+                <div id="grid",>
+                  { for (0..self.problem.size).map(|i: usize| view_row(i, &self.problem, &self.grid)) }
+                </div>
             </div>
         }
+    }
+}
+
+fn view_row(i: usize, prb: &Problem, grid: &Grid) -> Html<Model> {
+    let style = |i: usize, j: usize| {
+        if grid.selected == Some((i, j)) {
+            format!(
+                "background-color: hsla({}, 100%, 50%, 1);",
+                color(prb.values[i][j])
+            )
+        } else {
+            "background-color: #D3D3D3;".to_owned()
+        }
+    };
+    html! {
+        <div class="row",>
+           { for (0..prb.size).map(|j| html! { <div class="cell", style={ style(i, j) },></div> }) }
+        </div>
     }
 }
